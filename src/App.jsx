899f930bbc74805b41dd460aabf69f09558e3864
zena <meladzena23@gmail.com>
2025-05-17 21:12:51 +0300

    initail commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+
+
+import './App.css'
+
+function App() {
+
+
+  return (
+   <div className=' size-full'>
+    <h1 className='text-amber-600'>hello from react whit vite</h1>
+ <p className='w-1/2 bg-amber-500'>hi zena</p>
+     
+    </div>
+  )
+}
+
+export default App
